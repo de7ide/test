5b2db1fbcfcd8d180cd1cb826af755acc9a260d0
s8de <roman.kovalev2004@gmail.com>
2024-08-02 23:31:32 +0300

    cumeBack

--- testik.py
+++ testik.py
@@ -10,7 +10,7 @@
 #         self.name = name
 #         self.age = age
 #         self.email = email
-        
+
 
 # def get_user_info(user: User) -> str:
 #     return f"The user's age is {user.name} - {user.age}, "\
@@ -21,21 +21,22 @@
 # print(get_user_info(user_1))
 # ----------------------------------------
 
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
 
-@dataclass
-class User:
-    user_id: int
-    name: str
-    age: int
-    email: str
+# @dataclass
+# class User:
+#     user_id: int
+#     name: str
+#     age: int
+#     email: str
 
 
-def get_user_info(user: User) -> str:
-    return f"The user's age is {user.name} - {user.age}, "\
-        f"a email - {user.email}"
+# def get_user_info(user: User) -> str:
+#     return f"The user's age is {user.name} - {user.age}, "\
+#         f"a email - {user.email}"
 
 
-user_1: User = User(233452, "Sergei", 54, 'ser@gmail.com')
-print(get_user_info(user_1))
+# user_1: User = User(233452, "Sergei", 54, 'ser@gmail.com')
+# print(get_user_info(user_1))
+#--------------------------------
